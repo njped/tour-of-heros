4654d7e32ea0a58c1c3736d5322d91f043cc3930
Nathan Pedigo <njpedigo@gmail.com>
2023-08-21 07:25:28 -0600

    init the repo and started making the header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailsComponent } from './heroDetails/heroDetails.component';
+import { HeroListComponent } from './heroList/heroList.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      DashboardComponent,
+      HeroDetailsComponent,
+      HeroListComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/heroDetails/heroDetails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroDetails',
+  templateUrl: './heroDetails.component.html',
+  styleUrls: ['./heroDetails.component.css']
+})
+export class HeroDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
