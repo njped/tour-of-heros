7b6f71e29177af405cb38ec942ddf1f28c28a7b4
Nathan Pedigo <njpedigo@gmail.com>
2023-09-19 09:17:03 -0600

    finished step 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailsComponent } from './heroDetails/heroDetails.component';
 import { HeroesComponent } from './heroes/heroes.component';
+
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [			
