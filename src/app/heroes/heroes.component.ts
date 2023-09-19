039f5ab9baf8a7db681d8aa1052364b89b9c3b1b
Nathan Pedigo <njpedigo@gmail.com>
2023-09-19 10:26:12 -0600

    created a message service to send message of receiving hero details and sending a message of what the id is of the selected hero. Finished step 4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   }
 
-  heroes = HEROES
+  heroes: Hero[] = []
   selectedHero?: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService,
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.getHeroes()
+  }
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HerosComponent: Selected hero id=${hero.id}`)
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes)
   }
 }
